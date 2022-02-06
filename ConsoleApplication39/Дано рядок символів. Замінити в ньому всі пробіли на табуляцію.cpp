63d76a41407e08f3118efd6ab6426687e54e969b
Mike <Mike@DESKTOP-OMGOVC7>
2022-02-06 17:48:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication39/Дано рядок символів. Замінити в ньому всі пробіли на табуляцію.cpp
@@ -0,0 +1,25 @@
+﻿//1. Дан рядок символів. Замінити в ньому всі пробіли на табуляцію
+
+#include <iostream>
+#include <locale>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string text;
+	cout << "Будь ласка введіть текст для заміни пробілів на табуляцію... " << endl;	
+	getline(cin, text);		
+	int size = text.size();	
+	for (int i = 0; i < size; i++)
+	{
+		if (text[i] == ' ') {		
+			text[i] = '\t';	
+		}
+	}
+	cout << text << endl;
+}
+
